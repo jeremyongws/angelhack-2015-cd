997ae08fd74e41a45282babf2495f8c6b9499b22
Jeremy <jeremy.ongws@gmail.com>
2015-06-14 11:23:37 +0800

    final push, added TM CAAS Voice notification API

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -38,3 +38,17 @@ end
 get '/preview' do
   erb :issue_preview
 end
+
+get '/tmapi' do
+        RestClient.post("/https://58.27.83.230/rest/fastlogin/v1.0?app_key=vM8Vx1ndI4z9bYF7VIzQiUqYVaga&username=%2B601546010181&format=json",
+        {:Content-Type=>"application/json",
+         :Authorization=> "Huawei1234"}
+         )
+        
+
+
+       RestClient.post("https://58.27.83.230/sandbox/rest/httpsessions/tts2Note/v1.0?app_key=vM8Vx1ndI4z9bYF7VIzQiUqYVaga&access_token=71a7b135bb36936b15a77aeb30faa&format=json",
+
+        {"calleeNbr"=>"+601546010181","displayNbr"=>"+601546010181","languageType"=>"0","replayTimes"=>"10","ttsContent"=>"SGVsbG8gd29ybGQ="}
+        )
+end
